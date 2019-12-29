df1ffd5da560d69462dd68b0409d8bdae88ddc4a
Lin Trieu <linnatrieu@gmail.com>
2019-12-29 17:47:51 +0000

    Adds newDeck function to deck type

--- deck.go
+++ deck.go
@@ -5,6 +5,21 @@ import "fmt"
 // Create a new type of 'deck' which is a slice of strings
 type deck []string
 
+func newDeck() deck {
+	cards := deck{}
+
+	cardSuits := []string{"Spades", "Hearts", "Diamonds", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four"}
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+
+	return cards
+}
+
 func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card)
--- main.go
+++ main.go
@@ -1,12 +1,7 @@
 package main
 
 func main() {
-	cards := deck{"Ace of Diamonds", newCard()}
-	cards = append(cards, "Six of Spaces")
+	cards := newDeck()
 
 	cards.print()
-}
-
-func newCard() string {
-	return "Five of Diamonds"
 }
