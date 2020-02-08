0bf042f44503d344be22e7cf1b85f9b2c2267751
Lin Trieu <linnatrieu@gmail.com>
2020-02-08 12:53:57 +0000

    Completes exercise 33. Asserting Elements in a Slice

--- deck_test.go
+++ deck_test.go
@@ -8,4 +8,12 @@ func TestNewDeck(t *testing.T) {
 	if len(d) != 16 {
 		t.Errorf("Expected deck length of 16, but got %v", len(d))
 	}
+
+	if d[0] != "Ace of Spades" {
+		t.Errorf("Expected first card is Ace of Spades, but got %v", d[0])
+	}
+
+	if d[len(d)-1] != "Four of Clubs" {
+		t.Errorf("Expected last card is Four of Clubs, but got %v", d[len(d)-1])
+	}
 }
