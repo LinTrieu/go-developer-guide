d79c7cb62464ca8489d61a23c81b518fff82e1c0
Lin Trieu <linnatrieu@gmail.com>
2020-04-03 23:54:04 +0100

    Adds folder for Struct exercises

---
+++ structs/main.go
@@ -0,0 +1,55 @@
+package main
+
+import "fmt"
+
+type person struct {
+	firstName string
+	lastName  string
+	// embed a struct type as a property of another struct
+	contact contactInfo
+	// do not have to specify field name if we don't want to
+	// contactInfo
+}
+
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
+func main() {
+	// approach 1
+	// tim := person{"Tim", "Thomas"}
+	// fmt.Printf("%+v", tim)
+
+	// approach 2
+	george := person{firstName: "George", lastName: "Giant"}
+	fmt.Printf("%+v", george)
+
+	//approach 3∏
+	var alex person
+	alex.firstName = "Alex"
+	alex.lastName = "Anderson"
+	alex.contact.email = "alex@gmail.com"
+	alex.contact.zipCode = 1234
+	fmt.Printf("%+v", alex)
+
+    //approach 4
+    jim := person{
+        firstName: "Jim",
+        lastName: "Smith",
+        contact: contactInfo{
+            email: "jim@gmail.com",
+            zipCode: 1234,
+        },
+    }
+   jim.updateName("Jimmy")
+   jim.print()
+}
+
+func (p person) updateName(newFirstName string) {
+    p.firstName = newFirstName
+}
+
+func (p person) print() {
+    fmt.Printf("%+v", p)
+}
